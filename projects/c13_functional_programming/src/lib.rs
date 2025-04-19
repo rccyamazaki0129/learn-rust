ac1fed17c1f8734e46eab1b32f6fab79c8276a41
rei_yamazaki <rccyamazaki0129@gmail.com>
2025-04-19 14:50:38 +0900

    c13:iterator

--- projects/c13_functional_programming/src/lib.rs
+++ projects/c13_functional_programming/src/lib.rs
@@ -85,3 +85,99 @@ mod test {
         assert_eq!(v1, "Hello");
     }
 }
+
+#[cfg(test)]
+mod iterator_test {
+    #[test]
+    fn イテレータのテスト(){
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn イテレータを使って合計値を算出する() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn mapとcollectを使って配列に処理を行う() {
+        let v1 = vec![1, 2, 3];
+        let result: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+        assert_eq!(result, vec![2, 3, 4], "{:?}", result);
+    }
+
+    #[derive(PartialEq, Debug)]
+    struct Shoe {
+        size: u32,
+        style: String
+    }
+
+    fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+        shoes.into_iter().filter(|x|shoe_size == x.size).collect()
+    }
+
+    #[test]
+    fn 靴の配列から自分のサイズのものだけ取り出す() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker")},
+            Shoe { size: 13, style: String::from("sandal")},
+            Shoe { size: 10, style: String::from("boot")},
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(in_my_size, [
+            Shoe { size: 10, style: String::from("sneaker")},
+            Shoe { size: 10, style: String::from("boot")},
+        ]);
+    }
+
+    #[derive(Debug)]
+    struct Counter { count: i32
+    }
+
+    impl Counter {
+        fn new() -> Counter {
+            Counter { count: 0 }
+        }
+    }
+    
+    impl Iterator for Counter {
+        type Item = i32;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.count += 1;
+            if self.count < 6 {
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+    
+
+    #[test]
+    fn 独自のイテレータで5まで数える() {
+        let mut counter = Counter::new();
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn 別のイテレータを使う() {
+        let sum: i32 = Counter::new().zip(Counter::new().skip(1)).map(|(x,y)|x * y).filter(|x|x % 3 == 0).sum();
+        assert_eq!(18, sum);
+    }
+}
+
--- projects/c13_functional_programming/src/main.rs
+++ projects/c13_functional_programming/src/main.rs
@@ -1,18 +1,29 @@
 use c13_functional_programming as MyModule;
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    if false {
+        let simulated_user_specified_value = 10;
+        let simulated_random_number = 7;
 
-    MyModule::generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number
-    );
+        MyModule::generate_workout(
+            simulated_user_specified_value,
+            simulated_random_number
+        );
+    }
 
     {
+        // Closures capture their environment
         let x = vec![1, 2, 3];
         let equal_to_x = move |z| z == x;
 
         let y  = vec![1, 2, 3];
         assert!(equal_to_x(y));
+    }
+
+    {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        for val in v1_iter {
+            println!("{}, ", val);
+        }
     }
 }
